47bb617c37a079d4dc0332323b98a5c39c7f2ee0
Nicolas Dupouy <nicolasdupouy@gmail.com>
2017-07-24 21:27:57 +0200

    Display a basic list

--- front/mobile/Android/PasswordStorage/mobile/src/main/java/com/ndu/passwordstorage/DisplayListActivity.java
+++ front/mobile/Android/PasswordStorage/mobile/src/main/java/com/ndu/passwordstorage/DisplayListActivity.java
@@ -1,13 +1,32 @@
 package com.ndu.passwordstorage;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class DisplayListActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class DisplayListActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_list);
+
+        ListView listView = getListView();
+        List<String> names = Arrays.asList("Nicolas", "Hanane", "Nora");
+
+        ArrayAdapter<String> namesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
+        listView.setAdapter(namesAdapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Log.v("TEST", "position = " + position + " / id = " + id);
     }
 }
