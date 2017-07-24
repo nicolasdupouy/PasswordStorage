c721e5767a457e5946acc6543f37987420af7998
Nicolas Dupouy <nicolasdupouy@gmail.com>
2017-07-25 00:39:52 +0200

    List entries are read from the PasswordDatas class

--- front/mobile/Android/PasswordStorage/mobile/src/main/java/com/ndu/passwordstorage/screen/DisplayListActivity.java
+++ front/mobile/Android/PasswordStorage/mobile/src/main/java/com/ndu/passwordstorage/screen/DisplayListActivity.java
@@ -8,11 +8,17 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.ndu.passwordstorage.R;
+import com.ndu.passwordstorage.data.PasswordDatas;
+import com.ndu.passwordstorage.data.impl.PasswordDatasImpl;
+import com.ndu.passwordstorage.model.PasswordEntry;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DisplayListActivity extends ListActivity {
+
+    private static final PasswordDatas passwordDatas = new PasswordDatasImpl();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +26,12 @@ public class DisplayListActivity extends ListActivity {
         setContentView(R.layout.activity_display_list);
 
         ListView listView = getListView();
-        List<String> names = Arrays.asList("Nicolas", "Hanane", "Nora");
+
+        List<PasswordEntry> passwordEntries = passwordDatas.readDatas();
+        List<String> names = new ArrayList<>();
+        for (PasswordEntry entry: passwordEntries) {
+            names.add(entry.getLogin());
+        }
 
         ArrayAdapter<String> namesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
         listView.setAdapter(namesAdapter);
