df59fcc522edbfdd81188a7ae43e9df09e4f78fe
Nicolas Dupouy (MacBook Pro) <nicolasdupouy@gmail.com>
2017-11-07 08:19:08 +0100

    Instantiate database helper in singleton main class

--- front/mobile/Android/PasswordStorage/mobile/src/main/java/com/ndu/passwordstorage/MainApp.java
+++ front/mobile/Android/PasswordStorage/mobile/src/main/java/com/ndu/passwordstorage/MainApp.java
@@ -2,16 +2,19 @@ package com.ndu.passwordstorage;
 
 import android.app.Application;
 
+import com.ndu.passwordstorage.data.DbHelper;
 import com.ndu.passwordstorage.data.PasswordDatas;
 import com.ndu.passwordstorage.data.impl.PasswordDatasImpl;
 
 public final class MainApp extends Application {
 
     private static PasswordDatas passwordDatas;
+    private static DbHelper dbHelper;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        dbHelper = new DbHelper(getApplicationContext());
         passwordDatas = new PasswordDatasImpl();
     }
 
