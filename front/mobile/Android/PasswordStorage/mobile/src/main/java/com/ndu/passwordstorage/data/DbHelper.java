2131a01c423f7ec25bc072e62d517d35a8db2b04
Nicolas Dupouy (MacBook Pro) <nicolasdupouy@gmail.com>
2017-11-13 23:35:40 +0100

    Define a database contrat and its implementation

--- front/mobile/Android/PasswordStorage/mobile/src/main/java/com/ndu/passwordstorage/data/DbHelper.java
+++ front/mobile/Android/PasswordStorage/mobile/src/main/java/com/ndu/passwordstorage/data/DbHelper.java
@@ -1,27 +1,12 @@
 package com.ndu.passwordstorage.data;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import com.ndu.passwordstorage.model.PasswordEntry;
 
-public class DbHelper extends SQLiteOpenHelper {
-    public DbHelper(Context context) {
-        super(context, DataContract.DATABASE_NAME, null, DataContract.DATABASE_VERSION);
-    }
+import java.util.List;
 
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(DataContract.DataEntry.SQL_CREATE_ENTRIES);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL(DataContract.DataEntry.SQL_DELETE_ENTRIES);
-        onCreate(sqLiteDatabase);
-    }
-
-    @Override
-    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        super.onDowngrade(db, oldVersion, newVersion);
-    }
+public interface DbHelper {
+    List<PasswordEntry> getEntries();
+    void insertEntry();
+    void updateEntry();
+    void deleteEntry();
 }
---
+++ front/mobile/Android/PasswordStorage/mobile/src/main/java/com/ndu/passwordstorage/data/DbHelperImpl.java
@@ -0,0 +1,52 @@
+package com.ndu.passwordstorage.data;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.ndu.passwordstorage.model.PasswordEntry;
+
+import java.util.List;
+
+public class DbHelperImpl extends SQLiteOpenHelper implements DbHelper {
+    public DbHelperImpl(Context context) {
+        super(context, DataContract.DATABASE_NAME, null, DataContract.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(DataContract.DataEntry.SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(DataContract.DataEntry.SQL_DELETE_ENTRIES);
+        onCreate(sqLiteDatabase);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        super.onDowngrade(db, oldVersion, newVersion);
+    }
+
+    @Override
+    public List<PasswordEntry> getEntries() {
+        return null;
+    }
+
+    @Override
+    public void insertEntry() {
+
+    }
+
+    @Override
+    public void updateEntry() {
+
+    }
+
+    @Override
+    public void deleteEntry() {
+
+    }
+}
