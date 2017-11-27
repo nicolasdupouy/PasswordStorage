124ef138b54847b980d4c43934c4277fcc7a919c
Nicolas Dupouy (MacBook Pro) <nicolasdupouy@gmail.com>
2017-11-27 17:21:17 +0100

    Create first test for database helper

---
+++ front/mobile/Android/PasswordStorage/mobile/src/test/java/com/ndu/passwordstorage/data/DbHelperImplTest.java
@@ -0,0 +1,41 @@
+package com.ndu.passwordstorage.data;
+
+import com.ndu.passwordstorage.model.PasswordEntry;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.robolectric.RobolectricTestRunner;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+public class DbHelperImplTest {
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+    @Mock
+    public DbHelper databaseMock;
+
+    private static PasswordEntry PASSWORD_ENTRY_1 = PasswordEntry.makeNew("site_1", "login_1", "password_1");
+
+    @Test
+    public void should_add_entry_on_empty_database() {
+        List<PasswordEntry> entriesBefore = databaseMock.getEntries();
+        databaseMock.insertEntry(PASSWORD_ENTRY_1);
+        List<PasswordEntry> entriesAfter = databaseMock.getEntries();
+
+        assertThat(entriesBefore.size(), is(0));
+        assertThat(entriesAfter.size(), is(1));
+        assertTrue(entriesAfter.contains(PASSWORD_ENTRY_1));
+    }
+}
